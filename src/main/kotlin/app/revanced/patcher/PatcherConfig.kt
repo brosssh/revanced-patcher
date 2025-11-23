d7922404bb9e769a034bd14d45b6600cc0f7a073
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2025-11-23 15:22:24 +0200

    Revert "build: Bump dependencies"
    This reverts commit fe5fb736

--- src/main/kotlin/app/revanced/patcher/PatcherConfig.kt
+++ src/main/kotlin/app/revanced/patcher/PatcherConfig.kt
@@ -16,28 +16,9 @@ import java.util.logging.Logger
 class PatcherConfig(
     internal val apkFile: File,
     private val temporaryFilesPath: File = File("revanced-temporary-files"),
-    aaptBinaryPath: File? = null,
+    aaptBinaryPath: String? = null,
     frameworkFileDirectory: String? = null,
 ) {
-    /**
-     * The configuration for the patcher.
-     *
-     * @param apkFile The apk file to patch.
-     * @param temporaryFilesPath A path to a folder to store temporary files in.
-     * @param aaptBinaryPath A path to a custom aapt binary.
-     * @param frameworkFileDirectory A path to the directory to cache the framework file in.
-     */
-    @Deprecated(
-        "Use the constructor with a File for aaptBinaryPath instead.",
-        ReplaceWith("PatcherConfig(apkFile, temporaryFilesPath, aaptBinaryPath?.let { File(it) }, frameworkFileDirectory)"),
-    )
-    constructor(
-        apkFile: File,
-        temporaryFilesPath: File = File("revanced-temporary-files"),
-        aaptBinaryPath: String? = null,
-        frameworkFileDirectory: String? = null,
-    ) : this(apkFile, temporaryFilesPath, aaptBinaryPath?.let { File(it) }, frameworkFileDirectory)
-
     private val logger = Logger.getLogger(PatcherConfig::class.java.name)
 
     /**
@@ -52,7 +33,8 @@ class PatcherConfig(
      */
     internal val resourceConfig =
         Config.getDefaultConfig().apply {
-            aaptBinary = aaptBinaryPath
+            useAapt2 = true
+            aaptPath = aaptBinaryPath ?: ""
             frameworkDirectory = frameworkFileDirectory
         }
 
